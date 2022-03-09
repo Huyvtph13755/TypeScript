1ee19317a758719da0ca6ca64d93d0afecc5b350
Huy <huyvtph13755@fpt.edu.vn>
2022-03-09 15:20:25 +0700

    update

---
+++ my-app/src/components/ShowInfo/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const ShowInfo = (props) => {
+    return <p>Thông tin user {props.name}</p>
+}
+export default ShowInfo;
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -8,12 +8,4 @@ const myStatus = false;
 const showInfo = (props) => {
   return <p>Thông tin user {props.name}</p>
 }
-const ShowInfo = (props) => {
-  return <p>Thông tin user {props.name}</p>
-}
-ReactDOM.render(<div><h1>Hello</h1>
-  <p>{myAge}</p>
-  <p>{myStatus ? "Đã kết hôn" : "Chưa kết hôn"}</p>
-  {showInfo({ name: "Hoang" })}
-  <ShowInfo name="Hoang" />
-</div>, document.querySelector("#root"))
+ReactDOM.render(<App/>, document.querySelector("#root"))
