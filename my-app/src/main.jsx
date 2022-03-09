903654ad76469de9d7b3cfaf8cc8d72fadfe594c
Huy <huyvtph13755@fpt.edu.vn>
2022-03-09 14:57:13 +0700

    update

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const myName = "Vũ Trọng Huy";
+const myAge = 20;
+const myStatus = false;
+const showInfo = (props) => {
+  return <p>Thông tin user {props.name}</p>
+}
+const ShowInfo = (props) => {
+  return <p>Thông tin user {props.name}</p>
+}
+ReactDOM.render(<div><h1>Hello</h1>
+  <p>{myAge}</p>
+  <p>{myStatus ? "Đã kết hôn" : "Chưa kết hôn"}</p>
+  {showInfo({ name: "Hoang" })}
+  <ShowInfo name="Hoang" />
+</div>, document.querySelector("#root"))
