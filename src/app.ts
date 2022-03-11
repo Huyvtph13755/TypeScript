f70a23e52b0cbc38f6dd805b75abda65cdcd9a0e
Huy <huyvtph13755@fpt.edu.vn>
2022-03-11 15:04:48 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -16,23 +16,47 @@
 // }else{
 //     console.log("Not show result");
 // }
-type TPerson = {
-    id: number;
+// type TPerson = {
+//     id: number;
+//     name: string;
+//     age: number;
+//     status?: boolean;
+// }
+// const person: TPerson = {
+//     id: 1,
+//     name: "Huy",
+//     age: 34
+// } 
+// const persons: TPerson[] = [
+//     {id: 1, name: "Huy3", age: 34, status: true},
+//     {id: 2, name: "Huy2", age: 35, status: false},
+// ];
+// console.log(person.name);
+
+export type User = {
     name: string;
     age: number;
-    status?: boolean;
-}
-const person: TPerson = {
-    id: 1,
-    name: "Huy",
-    age: 34
-} 
-const persons: TPerson[] = [
-    {id: 1, name: "Huy3", age: 34, status: true},
-    {id: 2, name: "Huy2", age: 35, status: false},
+    occupation: string;
+};
+
+export const users: User[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
 ];
-console.log(person.name);
 
+export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
 
+console.log('Users:');
+users.forEach(logPerson);
 
 
