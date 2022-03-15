f9ff6c9c161d9834b148d3a30a7ac8af2acb61b2
Huy <huyvtph13755@fpt.edu.vn>
2022-03-15 21:50:13 +0700

    fix

--- my-app/src/ShowInfo.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ShowInfo = (props) => {
-  return (
-    <div>ShowInfo <br /> {props.name}</div>
-  )
-}
-
-export default ShowInfo
--- my-app/src/types/product.tsx
+++
@@ -1,5 +0,0 @@
-export type ProductType = {
-    id: number,
-    name: string, 
-    age: number
-}
